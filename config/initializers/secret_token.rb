7a4c499c134cb4a669f96f27abd2ddc897a60454
erlaroche <E.R.Laroche@gmail.com>
2014-02-12 13:48:05 -0600

    Initial commit, Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BirdWatch::Application.config.secret_key_base = '025d2786319c1a9c6850619b83b0c1d05e8b62222656879ba4bc45b8bdd6535c6f63105bb62956e1d04b6c7b68c3030a2f314ac8f4b62d475cd30ac1f6bac609'
