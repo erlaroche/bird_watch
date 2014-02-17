6487527392ccec886ddeff93ae439c3e7d9d18d9
erlaroche <E.R.Laroche@gmail.com>
2014-02-17 17:34:10 -0600

    Add gem hirb for console

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,2 +1,6 @@
 class Clip < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 5 }
 end
+
+Clip.create(title: "Pigmy Nuthatch"). valid? # => true
+Clip.create(title: nil).valid? # => falsle
