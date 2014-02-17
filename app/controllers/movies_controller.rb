5bf501671b3b41c8f47b14008dbe4f372c8f0491
erlaroche <E.R.Laroche@gmail.com>
2014-02-17 14:37:51 -0600

    Save data in controller and show new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,8 +9,19 @@ class MoviesController < ApplicationController
   def new
   end
 
-  def create
-    render text: params[:movie].inspect  
+  def show
+    @movie = Movie.find(params[:id])
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+
+    @movie.save
+    redirect_to @movie 
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :text)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216210301) do
+ActiveRecord::Schema.define(version: 20140217201227) do
 
   create_table "clips", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "movies", force: true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
