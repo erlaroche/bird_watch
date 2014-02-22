6e93bb7862bf4b04e44aed9b3276b830deb6fdb1
erlaroche <E.R.Laroche@gmail.com>
2014-02-21 23:35:39 -0600

    Add route for bluejay and add to view of featured birds

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ BirdWatch::Application.routes.draw do
   get "/clips/all" => "clips#show_all"
   get "/clips/mockingbird" => "clips#show_mockingbird"
   get "/clips/tuftedtitmouse" => "clips#show_tuftedtitmouse"
+  get "/clips/bluejay" => "clips#show_bluejay"
   # get "<url-slug>" => "controller_name#action"
   resources :clips do
     resources :remarks
