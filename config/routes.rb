b1ba359d161fa8305b4b058ee3aec673b896e384
erlaroche <E.R.Laroche@gmail.com>
2014-02-26 12:03:15 -0600

    Add named routes and alter new.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 BirdWatch::Application.routes.draw do
   devise_for :users
-  get "welcome/index"
+  get "welcome/index", :as => 'home'
   
-  get "/clips/all" => "clips#show_all"
-  get "/clips/mockingbird" => "clips#show_mockingbird"
-  get "/clips/tuftedtitmouse" => "clips#show_tuftedtitmouse"
-  get "/clips/bluejay" => "clips#show_bluejay"
+  get "/clips/all" => "clips#show_all", :as => 'featured_birds'
+  get "/clips/mockingbird" => "clips#show_mockingbird", :as => 'mockingbird'
+  get "/clips/tuftedtitmouse" => "clips#show_tuftedtitmouse", :as => 'tuftedtitmouse'
+  get "/clips/bluejay" => "clips#show_bluejay", :as => 'bluejay'
   # get "<url-slug>" => "controller_name#action"
   resources :clips do
     resources :remarks
