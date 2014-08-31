a95477bd5dce325c9be8e2fd13658c3e57b4cecd
Emma Laroche <E.R.Laroche@gmail.com>
2014-08-31 01:08:05 -0500

    Add link in /clips to individual entries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
 	def index
 	end
+	def show
+		@user = User.find(params[:id])
+	end
+	def list
+		@users = User.find(:all)
+	end
 end
--- app/models/clip.rb
+++ app/models/clip.rb
@@ -6,21 +6,6 @@ class Clip < ActiveRecord::Base
   validates :youtube_id, presence: true, length: { minimum: 5 }
   validates :text, presence: true, length: { minimum: 10 }
 
-  # before_create -> do
-  #   uid = link.match(YT_LINK_FORMAT)
-  #   self.uid = uid[2] if uid && uid[2]
-
-  #   if self.uid.to_s.length != 11
-  #     self.errors.add(:link, 'is invalid.')
-  #     false
-  #   elsif Video.where(uid: self.uid).any?
-  #     self.errors.add(:link, 'is not unique.')
-  #     false
-  #   else
-  #     get_additional_info
-  #   end
-  # end
-
   private
 
   def get_additional_info
