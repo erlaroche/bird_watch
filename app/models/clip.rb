9cd082dccb20e34895c3f585a3fd5c5d47aec7ac
Emma Laroche <E.R.Laroche@gmail.com>
2014-08-31 02:58:57 -0500

    Remove public key

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -6,12 +6,6 @@ class Clip < ActiveRecord::Base
     self.text.truncate(40)
   end
 
-  def youtube_query
-     response = Unirest.get("https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id=020sZ0PY8ko&key={AIzaSyBt8CXXq__mHV3zy-PCw7SaFAgjre44nIA}")
-     self.text = response.body["items"][0]['snippet']['text']
-     self.save
-   end
-
   validates :title, presence: true, length: { minimum: 5 }
   validates :youtube_id, presence: true, length: { minimum: 5 }
   validates :text, presence: true, length: { minimum: 10 }
