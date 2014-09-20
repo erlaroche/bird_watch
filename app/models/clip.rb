662063674302b9690ad780f998eed1bb4c1497ad
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-20 15:58:59 -0500

    Add comment to clip.rb, need to add max characters to title youtube_id, and text.

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -5,7 +5,7 @@ class Clip < ActiveRecord::Base
   def snippet
     self.text.truncate(40)
   end
-
+	# add maximun characters
   validates :title, presence: true, length: { minimum: 5 }
   validates :youtube_id, presence: true, length: { minimum: 5 }
   validates :text, presence: true, length: { minimum: 10 }
